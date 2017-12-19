34045b482fce8bf19d4d9f2051b6794771ec0114
Namec <christian.bruene@stud.tu-darmstadt.de>
2017-12-19 12:06:24 +0100

    Created and added Pre Shared Keys

--- Cryptographer.py
+++ Cryptographer.py
@@ -10,12 +10,17 @@ class Cryptographer(asyncore.dispatcher):
         self.preSharedKey = ''
         self.create_socket()
         self.connect((host, port))
-        print('Cryptographer %s running' % name)
+        print('Cryptographer %s running ' % name)
         self.send(str.encode('%s is here' % name))
+        self.name = name
+        self.psk = (0, 0)
 
     def handle_read(self):
         data = self.recv(8192)
         print(data)
+
+    def setPSK(self, psk):
+        self.psk = psk
 
 
 # if __name__ == '__main__':
@@ -46,9 +51,22 @@ def main(argv):
     print('Output file is: ', optionalPar)
 
 
+def calcPSK(name, name2):
+    return (42 * ord(name) + 73 * ord(name2)) % 1337
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
     c0 = Cryptographer("127.0.0.1", 1338, "A")
     c1 = Cryptographer("127.0.0.1", 1338, "B")
     c2 = Cryptographer("127.0.0.1", 1338, "C")
-    #asyncore.loop()
+
+    # create PSKs
+    psk01 = calcPSK(c0.name, c1.name)
+    psk02 = calcPSK(c0.name, c2.name)
+    psk12 = calcPSK(c1.name, c2.name)
+    c0.setPSK((psk01, psk02))
+    c1.setPSK((psk01, psk12))
+    c2.setPSK((psk02, psk12))
+
+    asyncore.loop()

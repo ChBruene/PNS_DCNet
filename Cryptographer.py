84b6e6241d2cbb0f5ba900c8a7e49bd039f66809
Namec <christian.bruene@stud.tu-darmstadt.de>
2017-12-19 10:01:20 +0100

    select subtask with parameter (t1,t2,...) and 3 Cryptographers created (reverted from commit a666c09e2a337796b3131709dad9fbab8c660587)

--- Cryptographer.py
+++ Cryptographer.py
@@ -1,5 +1,4 @@
-import asyncore, sys
-
+import asyncore
 
 class Cryptographer(asyncore.dispatcher):
 
@@ -7,9 +6,6 @@ class Cryptographer(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket()
         self.connect((host, port))
-        self.name = name
-        self.key = (0, 0)
-        self.message = 0
         print('Cryptographer %s running' % name)
         self.send(str.encode('%s is here' % name))
 
@@ -17,43 +13,6 @@ class Cryptographer(asyncore.dispatcher):
         data = self.recv(8192)
         print(data)
 
-    def dcEncrypt(self):
-        return self.key[0] ^ self.key[1] ^ self.message
-
-
 if __name__ == '__main__':
-
-    param = sys.argv[1]
-
-    cA = Cryptographer("127.0.0.1", 1338, "A")
-    cB = Cryptographer("127.0.0.1", 1338, "B")
-    cC = Cryptographer("127.0.0.1", 1338, "C")
-
-    asyncore.loop()  # ???
-    if param == 't1':  # Task 1
-        ### create pre-shared-key
-        kAB = (ord(cA.name) + ord(cB.name) * 42) % 1000
-        kAC = (ord(cA.name) + ord(cC.name) * 42) % 1000
-        kBC = (ord(cB.name) + ord(cC.name) * 42) % 1000
-        cA.key = (kAB, kAC)
-        cB.key = (kAB, kBC)
-        cC.key = (kAC, kBC)
-        ### Read/Set the message
-
-        ### Send messages
-
-        ### Receive public known message
-    elif param == 't2':
-        sendInterval = 3  # seconds
-        # code here
-    elif param == 't3':
-        print(42)
-        # code
-    elif param == 't4':
-        print(42)
-        # code
-    elif param == 't5':
-        print(42)
-        # code
-    else:
-        print('Cryptographer main switch-case-dafault')
+    c = Cryptographer("127.0.0.1", 1338, "A")
+    asyncore.loop();
